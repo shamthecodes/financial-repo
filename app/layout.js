fa682bc1f8e29a2d4106818a0287d5e1c68a066f
shamthecodes <shamthecoder@gmail.com>
2025-04-18 05:39:34 +0530

    Prepare for subset

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Wealth",
+  title: "Wealth", // corrected title spelling
   description: "One stop Finance Platform",
 };
 
@@ -17,15 +17,18 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <head>
           <link rel="icon" href="/logo-sm.png" sizes="any" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
         </head>
-        <body className={`${inter.className}`}>
+        <body className={inter.className}>
           <Header />
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
-
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with by Shamthecoder</p>
+              <p>Made with 💗 by Shamthecoder</p>
             </div>
           </footer>
         </body>
