7d660993d434b90aab63f463ed7a11ea005ec813
shamthecodes <shamthecoder@gmail.com>
2025-04-18 05:36:02 +0530

    Prepare for subset

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
-const inter = Inter({ subsets: ["latin"] }); // fixed typo here
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Wealth",
@@ -15,15 +15,17 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
         <body className={`${inter.className}`}>
-          {/* Header */}
           <Header />
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
-          {/* footer */}
+
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with by shamthecoder</p>
+              <p>Made with by Shamthecoder</p>
             </div>
           </footer>
         </body>
